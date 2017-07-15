5827954c1c7e1159f72a0b7de694aab140a368e0
zaarthak <ssarthakgrover365@gmail.com>
2017-07-15 17:35:52 +0530

    Minor changes

--- app/build/generated/source/r/debug/com/example/sarthak/news/R.java
+++ app/build/generated/source/r/debug/com/example/sarthak/news/R.java
@@ -3267,9 +3267,14 @@ containing a value of this type.
         public static final int item_touch_helper_max_drag_scroll_per_frame=0x7f080073;
         public static final int item_touch_helper_swipe_escape_max_velocity=0x7f080074;
         public static final int item_touch_helper_swipe_escape_velocity=0x7f080075;
-        public static final int notification_large_icon_height=0x7f080076;
-        public static final int notification_large_icon_width=0x7f080077;
-        public static final int notification_subtext_size=0x7f080078;
+        public static final int navigation_drawer_top_margin=0x7f080076;
+        public static final int news_image_min_height=0x7f080077;
+        public static final int notification_large_icon_height=0x7f080078;
+        public static final int notification_large_icon_width=0x7f080079;
+        public static final int notification_subtext_size=0x7f08007a;
+        public static final int settings_logout_start_end_margin=0x7f08007b;
+        public static final int settings_logout_top_margin=0x7f08007c;
+        public static final int textview_margin=0x7f08007d;
     }
     public static final class drawable {
         public static final int abc_ab_share_pack_mtrl_alpha=0x7f020000;
--- app/build/generated/source/r/debug/me/henrytao/smoothappbarlayout/R.java
+++ app/build/generated/source/r/debug/me/henrytao/smoothappbarlayout/R.java
@@ -569,9 +569,9 @@ public final class R {
         public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f080073;
         public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f080074;
         public static final int item_touch_helper_swipe_escape_velocity = 0x7f080075;
-        public static final int notification_large_icon_height = 0x7f080076;
-        public static final int notification_large_icon_width = 0x7f080077;
-        public static final int notification_subtext_size = 0x7f080078;
+        public static final int notification_large_icon_height = 0x7f080078;
+        public static final int notification_large_icon_width = 0x7f080079;
+        public static final int notification_subtext_size = 0x7f08007a;
     }
     public static final class drawable {
         public static final int abc_ab_share_pack_mtrl_alpha = 0x7f020000;
